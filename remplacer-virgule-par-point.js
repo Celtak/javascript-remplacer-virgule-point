5c117dcac9ee05d14c96a5e292987bef33069154
Henrique <rtrans.web@gmail.com>
2017-03-16 11:11:24 +0100

    Update remplacer-virgule-par-point.js

--- remplacer-virgule-par-point.js
+++ remplacer-virgule-par-point.js
@@ -1,8 +1,14 @@
-// insérez le nombre avec une virgule, la fonction le retournera avec un point de type numérique
-function remplacer_virgule_par_point(decimal)
-        {
-                    decimal = decimal+"";
-                    decimal = decimal.replace(",",".");
-                    decimal = parseFloat(decimal);
-                    return decimal; // renvoie le nombre décimal avec un point
-        }
+/**
+*
+* @param string decimal
+*
+* @return number
+*	return number with point
+*/
+
+function remplacer_virgule_par_point(decimal){
+	decimal = decimal+"";
+	decimal = decimal.replace(",",".");
+	decimal = parseFloat(decimal);
+	return decimal;
+}
